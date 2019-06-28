29fcd023a915eec34e785286a04b2a52998036ee
Aliazhari <Aliazhari@hotmail.com>
2019-06-28 16:39:16 -0400

    second test

--- test.py
+++ test.py
@@ -1,15 +1,6 @@
-import math
-import os
-import sys
+
 
 import requests
-
-print(sys.executable)
-test = 'hello'
-
-
-def greet(who_to_greet):
-    print('hello ' + who_to_greet)
 
 
 req = requests.get('https://github.com')
